9227a66eace988f7b0ba1399ff4c501dd77d0d0a
akshaysama <akshay.sivan@vonnue.com>
2022-10-16 18:44:47 +0530

    react typescript

---
+++ React Js TypeScript/react-typescript-demo/src/components/auth/Private.tsx
@@ -0,0 +1,15 @@
+// import {Login}  from './Login'
+// import React from "react"
+// import {ProfileProps} from "./Profile"
+// type PrivateProps ={
+//     isLoggedIn:boolean
+//     Component:React.ComponentType<ProfileProps>
+// }
+// export const Private =({isLoggedIn,Component}:PrivateProps)=>{
+//     if(isLoggedIn){
+//         return <Component name ="viswas" />
+//     }
+//     else{
+//         return <Login />
+//     }
+// }
